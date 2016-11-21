f889bea86896846e9f5e790ef57781775ad63859
kumar <kumar@r-ce-travelsoft-e0.net.ip.start.de>
2016-11-21 13:53:33 +0530

    Ticket Singleton : Creates a HealthKitManager Singleton for easy use and sharing same instance over multiple view controllers'

--- AmadeusFit1/HKHealthStore+AmadeusExtension.h
+++ AmadeusFit1/HKHealthStore+AmadeusExtension.h
@@ -8,6 +8,9 @@
 //  Easy to find the topmost stack of the data with predicate set to nil and completion block will tell the quantity from the samples. let's say 1,2,4,5,3 were the set of data entered , we just want to have the latest data, so thinking it as a stack we can pop 3.
 
 
+//The HealthKit store is encrypted when user's phone is locked. This means that you won't be able to read from it when your app is in the background. Writing to HealthKit, however, works even when the phone is locked.
+
+
 
 
 @import HealthKit;
