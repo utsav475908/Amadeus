5360361c7ec8867dbf0e1a6de2fecda599383628
checksumdeveloper <59607032+checksumdeveloper@users.noreply.github.com>
2020-01-07 17:58:34 +0530

    Update HealthKitManager.m

--- AmadeusFit1/HealthKitManager.m
+++ AmadeusFit1/HealthKitManager.m
@@ -22,6 +22,7 @@
     dispatch_once(&pred, ^{
         instance = [[HealthKitManager alloc]init];
         instance.healthStore = [[HKHealthStore alloc]init];
+        NSLog(@"Ab commit hoga");s
     });
     return instance;
 }
